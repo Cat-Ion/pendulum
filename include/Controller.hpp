1de97cb3ea615e9508cc8d44f73210452d247385
Valentin Ochs <a@0au.de>
2018-03-13 20:56:51 +0100

    Increased weights for angular velocities, somewhat more stable now

--- include/Controller.hpp
+++ include/Controller.hpp
@@ -12,7 +12,7 @@ public:
       p1 = fmod(p1, 2*M_PI);
       p2 = fmod(p2, 2*M_PI);
 
-      double weights[6]={-1.,-5.87,-1442, -164, 1502, 319};
+      double weights[6]={-0.316,-2.975,-1409,-165.0,1441,303.9};
       double u = weights[0]*x + weights[1]*dx + weights[2]*p1 + weights[3]*dp1 + weights[4]*p2 + weights[5]*dp2;
       printf("x:%6.3f dx:%6.3f phi:%6.3f dphi:%6.3f f:%10.3f\n", x, dx, p1, dp1, u);
       fflush(stdout);
